9729b1eb578a5a3d20809884c7425a57b42b35df
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-07-19 13:23:02 -1000

    cleanup

--- examples/old/pool_example/config/prod.exs
+++
@@ -1,8 +0,0 @@
-import Config
-
-# Production-specific configuration
-config :logger, level: :info
-
-config :pool_example,
-  pool_size: System.get_env("POOL_SIZE", "8") |> String.to_integer(),
-  overflow: System.get_env("POOL_OVERFLOW", "4") |> String.to_integer()
