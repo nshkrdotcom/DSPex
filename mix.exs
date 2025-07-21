4caeb7974d6de84d6b72e276161f69de174804aa
nshkrdotcom <ZeroTrust@NSHkr.com>
2025-07-20 20:33:30 -1000

    fb

--- mix.exs
+++ mix.exs
@@ -153,7 +153,7 @@ defmodule DSPex.MixProject do
         "GitHub" => "https://github.com/nshkrdotcom/dspex",
         "Documentation" => "https://hexdocs.pm/dspex"
       },
-      files: ~w(lib priv/python/*.py priv/plts assets .formatter.exs mix.exs README.md LICENSE CHANGELOG.md)
+      files: ~w(lib priv/python/*.py .formatter.exs mix.exs README.md LICENSE CHANGELOG.md)
     ]
   end
 
